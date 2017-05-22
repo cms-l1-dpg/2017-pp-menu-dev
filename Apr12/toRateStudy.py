1b32b6a62e07fce0f407a4771abb30bc4173bf82
Takashi MATSUSHITA <takashi.matsushita@cern.ch>
2017-05-23 01:41:11 +0200

    updating rs files

--- Apr12/toRateStudy.py
+++ Apr12/toRateStudy.py
@@ -13,7 +13,7 @@ ps_column = '2.0'
 import csvtool
 
 
-def menu_file(config):
+def menu_file(config, options):
   header = '''#============================================================================#          
 #-------------------------------     Menu     -------------------------------#          
 #============================================================================#          
@@ -23,8 +23,8 @@ def menu_file(config):
   output = file('menu.txt', 'w')
   output.write(header)
   for idx, name in config.algorithms.iteritems():
-    mask = config.masks[idx] if idx in config.masks else 0
-    output.write('{:60} {:>3}         {} {:6} {:6}\n'.format(name, idx, mask, config.POG[idx], config.PAG[idx]))
+    ps = 0 if (options.unprescaled and config.prescales[options.mask][idx] != 1) else config.prescales[options.mask][idx]
+    output.write('{:60} {:>3}         {} {:6} {:6}\n'.format(name, idx, ps, config.POG[idx], config.PAG[idx]))
   output.close()
 
   return
@@ -36,10 +36,11 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--csv", dest="csv", default=input_file, type=str, action="store", required=False, help="path to the configuration csv file [default: %s]" % input_file)
   parser.add_argument("--lumi", dest="mask", default=ps_column, type=str, action="store", required=False, help="prescale/mask column identifier [default: %s]" % ps_column)
+  parser.add_argument("--unprescaled", dest="unprescaled", default=False, action="store_true", required=False, help="use only unprescaled seeds")
 
   options = parser.parse_args()
   config = csvtool.parse(options.csv, [options.mask,])
 
-  menu_file(config)
+  menu_file(config, options)
 
 # end
